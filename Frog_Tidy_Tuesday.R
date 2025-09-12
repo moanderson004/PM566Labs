16283ac917bcadb8803881449fdef8ccc92491f2
Mitchell Anderson <moanders@usc.edu>
2025-09-12 13:15:01 -0700

    Lab 3 Anderson
    https://github.com/USCbiostats/PM566/issues/93

---
+++ Frog_Tidy_Tuesday.R
@@ -0,0 +1,8 @@
+1 of 2: "frogID_data.csv"
+2 of 2: "frog_names.csv"                                                                              
+> frogID_data <- tuesdata$frogID_data                                                                   
+> frog_names <- tuesdata$frog_names
+> 
+  > View(frogID_data)
+> plot(frogID_data$decimalLongitude, frogID_data$decimalLatitude)
+
